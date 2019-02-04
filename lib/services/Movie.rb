5c8349013e2b0c8382bf9a89abc97b0dc5b9a113
Tech-A42 <tech-a42@Tech-A42s-MacBook-Air.local>
2019-02-04 11:32:50 +0000

    Setup tests that check multiple datapoints

--- lib/services/Movie.rb
+++ lib/services/Movie.rb
@@ -14,7 +14,33 @@ class Movie
   end
 
   def get_status
-   @movie['status']
+    array=[]
+     array << @movie['status']
+   array
+  end
+
+  def get_copyright
+    @movie['copyright']
+  end
+
+  def get_results
+    @movie['results']
+  end
+  
+  def get_display_title
+    array = []
+    get_results.each do |t|
+     array << t['display_title']
+    end
+    array
+  end
+
+  def get_mpaa_rating
+    array = []
+    get_results.each do |m|
+      array <<m['mpaa_rating']
+    end
+    array
   end
 
   
@@ -23,3 +49,4 @@ class Movie
 
 
 end
+
--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -5,13 +5,32 @@ describe Movie_review do
 
   before(:all) do 
     @movies = Movie_review.new.movie_search
-    @movies.getdata('Aquaman')
+    @movies.getdata('star')
+   end
+   
+   it 'should return it as a hash' do 
+    expect(@movies).to be_kind_of(Object)
    end
 
-   
-  it 'should return it as a hash' do 
-    expect(@movies).to be_kind_of(Object)
+   it 'should return status as ok' do 
+    @movies.get_status.each do |s|
+      expect(s).to eq 'OK'
+    end
   end
- 
+
+   it 'should return copyright as a string' do
+    expect(@movies.get_copyright).to be_kind_of(String)
+   end
+
+   it 'should return result as an array' do 
+    expect(@movies.get_results).to be_kind_of(Array)
+   end
+
+   it 'should return display title as a string' do
+    @movies.get_display_title.each do |t|
+      expect(t).to be_kind_of(String)
+    end
+   end
+  
  end
 end 
